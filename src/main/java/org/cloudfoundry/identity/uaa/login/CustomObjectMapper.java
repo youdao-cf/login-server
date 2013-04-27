1cadb32dff78fd9fe9203b773314d48f1d04906e
Dachao <dugu108@gmail.com>
2013-04-27 11:48:12 +0800

    fix config of jackson

--- src/main/java/org/cloudfoundry/identity/uaa/login/CustomObjectMapper.java
+++ src/main/java/org/cloudfoundry/identity/uaa/login/CustomObjectMapper.java
@@ -2,16 +2,16 @@ package org.cloudfoundry.identity.uaa.login;
 
 import java.text.SimpleDateFormat;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+
 
 public class CustomObjectMapper extends ObjectMapper {
-	private static final long serialVersionUID = 8092264255887568235L;
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat(
 			"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
 	public CustomObjectMapper() {
-		configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
 		setDateFormat(dateFormat);
 	}
 
